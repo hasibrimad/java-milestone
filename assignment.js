d469334e18666fa3466a7ac189d39286dd4151e4
hasibrimad <hasibrimadfbi@gmail.com>
2020-07-17 23:54:45 +0600

    tried enough

--- assignment.js
+++ assignment.js
@@ -30,3 +30,24 @@ function tinyFriend(names){
 }
 var check = ['Ashfaq', 'Miha', 'Rahan', 'Labib', 'Newaz', 'Ashraf'];
 // console.log(tinyFriend(check));
+
+function brickCalculator(floor){
+    if(floor <=10){
+        var feet = 1000;
+        var bricks1 = feet * 15 * floor;
+        var total = bricks1;
+    }
+
+    else if(floor>=11 && floor <=20){
+        var bricks2 = feet * 12 * floor;
+        var total2 = bricks1 + bricks2
+    }
+    else if(floor >=21){
+        var bricks3 = feet * 10 * floor;
+        var total3 =bricks3 + total2;
+    }
+    return total3;
+    
+}
+var result = brickCalculator(23);
+console.log(result);
